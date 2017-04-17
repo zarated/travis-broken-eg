e66d6b621c39e4f93f6d26ab077d0b9c13f1bb78
zarated <paolo.zarate@gmail.com>
2017-04-17 15:09:25 -0400

    x

---
+++ bump.js
@@ -0,0 +1,18 @@
+t json = require('json-update')
+const fs = require('fs')
+
+const package = require('./package.json')
+
+let version = package.version.split('.')
+console.log('Previous version', version)
+version[version.length - 1] = Number(version[version.length - 1]) + 1
+version = version.join('.')
+
+json.update('./package.json' , {version})
+  .then(function(dat) {
+    console.log('Version bumped to ', version)
+});
+
+process.env['APP_VERSION'] = version
+
+fs.writeFileSync('./build_tracker', version, 'utf8')
