fbc11af46fc6065711ae7d0a7a7c9005260ca169
zarated <paolo.zarate@gmail.com>
2017-04-27 11:45:47 -0400

    x

--- main.js
+++ main.js
@@ -1,19 +1,17 @@
-// var webdriver = require('selenium-webdriver');
-// var capabilities = {
-//   'browserName' : 'firefox', 
-//   'browserstack.user' : '',
-//   'browserstack.key' : ''
-// }
-// var driver = new webdriver.Builder().
-//   usingServer('http://hub-cloud.browserstack.com/wd/hub').
-//   withCapabilities(capabilities).
-//   build();
-// driver.get('http://www.google.com');
-// driver.findElement(webdriver.By.name('q')).sendKeys('BrowserStack');
-// driver.findElement(webdriver.By.name('btnG')).click();
-// driver.getTitle().then(function(title) {
-//   console.log(title);
-// });
-// driver.quit();
-console.log(process.argv[0])
-console.log(process.argv[1])
+var webdriver = require('selenium-webdriver');
+var capabilities = {
+  'browserName' : 'firefox', 
+  'browserstack.user' : process.argv[0],
+  'browserstack.key' : process.argv[1]
+}
+var driver = new webdriver.Builder().
+  usingServer('http://hub-cloud.browserstack.com/wd/hub').
+  withCapabilities(capabilities).
+  build();
+driver.get('http://www.google.com');
+driver.findElement(webdriver.By.name('q')).sendKeys('BrowserStack');
+driver.findElement(webdriver.By.name('btnG')).click();
+driver.getTitle().then(function(title) {
+  console.log(title);
+});
+driver.quit();
